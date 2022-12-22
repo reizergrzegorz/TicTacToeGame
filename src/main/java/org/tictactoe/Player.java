578c223a64117033ee918c076c4b3c3da84e96a3
reizergrzegorz <reizergrzegorz@gmail.com>
2022-12-22 21:18:10 +0100

    Add Player class

---
+++ src/main/java/org/tictactoe/Player.java
@@ -0,0 +1,13 @@
+package org.tictactoe;
+
+public class Player {
+    private int playerNumber;
+
+    public Player(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+}
