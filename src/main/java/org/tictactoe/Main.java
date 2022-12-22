911e131941f13d438cb6a9c639ef7ede808abcda
reizergrzegorz <reizergrzegorz@gmail.com>
2022-12-22 22:07:07 +0100

    add object creation

--- src/main/java/org/tictactoe/Main.java
+++ src/main/java/org/tictactoe/Main.java
@@ -2,6 +2,33 @@ package org.tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Board board = new Board();
+        Display display = new Display();
+        Coordinates coordinates = new Coordinates();
+        Player player1 = new Player(1);
+        Player player2 = new Player(2);
+
+
+        int x = coordinates.changeColInputToIndex();
+        int y = coordinates.changeRowInputToIndex();
+        board.placePlayerChoice(x, y, player1);
+        display.displayBoard(board);
+        x = coordinates.changeColInputToIndex();
+        y = coordinates.changeRowInputToIndex();
+        board.placePlayerChoice(x, y, player2);
+        display.displayBoard(board);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
