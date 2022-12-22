42986725ea520a8e3e8cc09929d5daef89020042
reizergrzegorz <reizergrzegorz@gmail.com>
2022-12-22 20:51:27 +0100

    add Display class

---
+++ src/main/java/org/tictactoe/Display.java
@@ -0,0 +1,12 @@
+package org.tictactoe;
+
+public class Display {
+
+    public void displayBoard(Board board){
+        System.out.println(board);
+    }
+
+    public void displayMessage(String message){
+        System.out.println(message);
+    }
+}
